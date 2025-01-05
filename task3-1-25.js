af94bda734825a7895e24501800a542120ec4f2f
Dayanithi_E_K <dayanithiek.21cse@kongu.edu>
2025-01-05 23:09:19 +0530

    validation

--- task3-1-25.js
+++ task3-1-25.js
@@ -1,5 +1,8 @@
 const obj = {
   tag: "form",
+  attribute: {
+    autoComplete: "off",
+  },
   children: [
     {
       tag: "fieldset",
@@ -51,6 +54,7 @@ const obj = {
                 placeHolder: "9876543210",
                 id: "phone",
                 name: "phone",
+                minlength: "10",
                 required: true,
               },
               style: {
@@ -361,3 +365,141 @@ console.log(document.body);
 document.body.appendChild(final);
 
 const userName = document.getElementById("user-name");
+
+userName.addEventListener("keydown", (ele) => {
+  const pos = userName.selectionStart;
+  let name = userName.value;
+  // console.log(name);
+  if (
+    !(
+      (ele.key >= "a" && ele.key <= "z") ||
+      (ele.key >= "A" && ele.key <= "Z") ||
+      ele.key === " "
+    )
+  ) {
+    ele.preventDefault();
+  } else if (ele.key === " ") {
+    if (name === "" || name.at(pos - 1) === " " || name.at(pos) === " ")
+      ele.preventDefault();
+    else {
+      if (typeof name.at(pos) === "string")
+        userName.value = `${name.slice(0, pos)}${name
+          .at(pos)
+          .toUpperCase()}${name.slice(pos + 1)}`;
+      userName.setSelectionRange(pos, pos);
+    }
+  }
+  if (ele.key >= "a" && ele.key <= "z") {
+    if (name === "" || name.at(pos - 1) === " ") {
+      let end = name.slice(pos);
+      if (end.at(0) >= "A" && end.at(0) <= "Z")
+        end = end.at(0).toLowerCase() + end.slice(1);
+      userName.value = `${name.slice(0, pos)}${ele.key.toUpperCase()}${end}`;
+      ele.preventDefault();
+    }
+  }
+  // console.log(ele);
+});
+
+userName.addEventListener("keyup", (ele) => {
+  const pos = userName.selectionStart;
+  if (ele.key === "Backspace") {
+    let start = userName.value.slice(0, pos);
+    let end = userName.value.slice(pos);
+    if (start.at(-1) === " ") {
+      end = end.at(0).toUpperCase() + end.slice(1);
+    } else if (end.at(0) >= "A" && end.at(0) <= "Z") {
+      end = end.at(0).toLowerCase() + end.slice(1);
+    }
+    userName.value = `${start}${end}`;
+    userName.setSelectionRange(pos, pos);
+  }
+});
+
+userName.addEventListener("paste", (e) => {
+  e.preventDefault();
+});
+
+const phone = document.getElementById("phone");
+
+phone.addEventListener("paste", (e) => {
+  e.preventDefault();
+});
+
+phone.addEventListener("keydown", (e) => {
+  if (!((e.key >= "0" && e.key <= "9") || e.key === "Backspace")) {
+    e.preventDefault();
+  }
+  if (!(e.key === "Backspace") && phone.value.length >= 10) {
+    e.preventDefault();
+  }
+  if (e.key >= "0" && e.key <= "5" && phone.value === "") {
+    e.preventDefault();
+  }
+});
+
+const mail = document.getElementById("mail");
+
+mail.addEventListener("paste", (e) => {
+  e.preventDefault();
+});
+
+function isAlphanum(s) {
+  return (
+    (s >= "a" && s <= "z") || (s >= "A" && s <= "Z") || (s >= "0" && s <= "9")
+  );
+}
+
+let flag = false;
+const specialCharacters = [
+  "!",
+  "#",
+  "~",
+  "$",
+  "%",
+  "&",
+  `'`,
+  ".",
+  "/",
+  "=",
+  "^",
+  "-",
+  "_",
+  "@",
+];
+mail.addEventListener("keydown", (e) => {
+  if (!isAlphanum(e.key) && !specialCharacters.includes(e.key))
+    e.preventDefault();
+  if (e.key === "@" && mail.value !== "") {
+    if (!flag) {
+      flag = true;
+    } else e.preventDefault();
+    // console.log(flag);
+  }
+  if (e.key === "Backspace") {
+    if (mail.value.at(-1) === "@") {
+      flag = false;
+    }
+  } else if (!isAlphanum(e.key) && !isAlphanum(mail.value.at(-1))) {
+    e.preventDefault();
+  }
+});
+
+const submit = document.getElementById("submit");
+submit.addEventListener("mouseover", () => {
+  submit.style.backgroundColor = "green";
+  submit.style.color = "white";
+});
+submit.addEventListener("mouseleave", () => {
+  submit.style.backgroundColor = "#a4ffbe";
+  submit.style.color = "black";
+});
+const reset = document.getElementById("reset");
+reset.addEventListener("mouseenter", () => {
+  reset.style.backgroundColor = "rgb(211, 0, 0)";
+  reset.style.color = "white";
+});
+reset.addEventListener("mouseout", () => {
+  reset.style.backgroundColor = "rgb(253, 165, 165)";
+  reset.style.color = "black";
+});
